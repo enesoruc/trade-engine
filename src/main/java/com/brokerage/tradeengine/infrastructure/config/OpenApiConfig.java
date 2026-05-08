2b72ec226348fbf54ee96f564de3bb30d5e714e6
= <=>
2026-05-08 20:53:23 +0300

    chore: Add OpenApiConfig

---
+++ src/main/java/com/brokerage/tradeengine/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.brokerage.tradeengine.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "basicAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Trade Engine API")
+                        .version("1.0")
+                        .description("Brokerage Trade Engine API Documentation"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
+    }
+}
